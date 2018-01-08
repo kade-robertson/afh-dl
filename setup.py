f0ddf595ce1a385d7e923c66175e46c2418da108
Kade Robertson <kade.robertson16@gmail.com>
2018-01-08 10:40:47 -0500

    initial work, only interactive

--- setup.py
+++ setup.py
@@ -2,15 +2,30 @@ from setuptools import setup, find_packages
 
 import afh_dl
 
+long_desc = ""
+try:
+    import pypandoc
+    long_desc = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_desc = open('README.md').read()
+
 setup(
     name = "afh-dl",
-    version = "1.0.0.dev2",
+    version = "1.0.0a1",
     description = "A command-line tool for downloading files from AndroidFileHost.",
-    long_description = "",
+    long_description = long_desc,
     classifiers = [
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent",        
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6"
@@ -26,6 +41,11 @@ setup(
     url = "https://github.com/kade-robertson/afh-dl",
     license = "MIT",
     packages = find_packages(),
-    install_requires = [],
-    python_requires = '~=3.4',
+    install_requires = [
+        "future",
+        "requests",
+        "humanize",
+        "clint"
+    ],
+    python_requires = '>=2.7, <4',
 )
